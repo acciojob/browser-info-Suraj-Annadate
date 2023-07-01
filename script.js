3a0749862427e49a57a0748bff69b17df90a186b
Suraj Annadate <124056028+Suraj-Annadate@users.noreply.github.com>
2023-07-01 19:12:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let div=document.createElement("div");
 div.id='browser-info';
 document.body.append(div);
-div.innerHTML="You are using "+navigator.appName+" version "+navigator.appVersion;
+div.innerHTML="You are using "+ navigator.appName +" version "+navigator.appVersion;
